1b52ef9f33c331f80a60292c77be20a2c814f625
Honkytonkunderground <honkytonkunderground21@gmail.com>
2025-01-06 11:32:27 -0700

    Email change

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -28,7 +28,7 @@ export function Booking() {
             className="bg-[#2a2422] p-6 sm:p-8 rounded-lg hover:bg-[#332e2b] transition-colors text-left"
           >
             <Mail className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 mx-auto mb-4" />
-            <h3 className="text-xl sm:text-2xl font-bold mb-2 text-center">Email Us</h3>
+            <h3 className="text-xl sm:text-2xl font-bold mb-2 text-center">Email Dave</h3>
             {emailCopied && <div className="mb-2 text-sm text-green-500 text-center">Copied!</div>}
             <div className="text-base sm:text-lg text-gray-300 text-center hover:text-[#f59d0e] transition-colors overflow-hidden whitespace-nowrap text-ellipsis">
               honkytonkunderground21@gmail.com
